a6a849a8460d897fa5a5649609b7d4061fd2e127
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-06-10 12:01:54 +0530

    magnitude of vector

--- Tree.java
+++ Tree.java
@@ -1,19 +1,11 @@
-// Count toal node
-class Node {
-    int data;
-    Node left, right;
-
-    Node(int data){
-        this.data = data;
-        left = right = null;
-    }
-}
 public class Tree{
     public static void main(String args[]){
-        Node root = new Node(10);
-        root.left = new Node (20);
-        root.right = new Node(30);
+        int x = 3 ;
+        int y = 4;
 
-        System.out.println("Root = " + root.data);
+        double magnitude = Math.sqrt(x*x + y*y);
+
+        System.out.println("magnitude = " + magnitude);
+
     }
 }
