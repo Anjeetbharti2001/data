0a61919f6ccdceb559cbe6d016ee727f6d93cce7
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-06-10 08:53:35 +0530

    create a binary tree

--- Tree.java
+++ Tree.java
@@ -1,27 +1,19 @@
-// Height of Tree
-class Node{
+// Count toal node
+class Node {
     int data;
-    Node left,right;
+    Node left, right;
 
     Node(int data){
-     this.data = data;
-     left = right = null;
+        this.data = data;
+        left = right = null;
     }
 }
 public class Tree{
-    static int height(Node root){
-        if(root == null){
-            return 0;
-        }
-        return 1 + Math.max(height(root.left), height(root.right));
-    }
     public static void main(String args[]){
-       Node root = new Node(10);
-       root.left = new Node(20);
-       root.right = new Node(30);
-       root.left.left = new Node(40);
-       root.left.right = new Node(50);
+        Node root = new Node(10);
+        root.left = new Node (20);
+        root.right = new Node(30);
 
-       System.out.println("height = " + height(root));
+        System.out.println("Root = " + root.data);
     }
 }
