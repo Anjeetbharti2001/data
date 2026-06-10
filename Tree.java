29a44e7d6f08b75dbbb5e0c021bc0a1aa9e54ea9
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-06-10 08:04:45 +0530

    Count Total Node

--- Tree.java
+++ Tree.java
@@ -1,19 +1,24 @@
-class Node {
+class Node{
     int data;
     Node left, right;
 
     Node(int data){
         this.data = data;
-        left = right = null;
     }
 }
-
 public class Tree{
-    public static void main(String [] args){
-        Node root = new Node(10);
-        root.left = new Node(20);
-        root.right = new Node(30);
+    static int countNodes(Node root){
+        if(root == null)
+            return 0;
 
-        System.out.println("Root : " + root.data );
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+    public static void main(String args[]){
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        System.out.println("Total Nodes = " + countNodes(root));
+
     }
 }
