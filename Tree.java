1d3fc07e9d1dac56f46be1ecb72ce69bc5369719
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-06-10 08:28:50 +0530

    Preorder Traversal

--- Tree.java
+++ Tree.java
@@ -1,25 +1,29 @@
-// Sum of All Node
+// Preorder Traversal (Root -> left -> right)
 class Node{
     int data;
-    Node left, right; 
+    Node left, right;
 
     Node(int data){
         this.data = data;
         left = right = null;
     }
 }
-public class Tree {
-    static int sum(Node root){
+public class Tree{
+    static void preorder(Node root){
         if(root == null){
-            return 0;
+            return ;
         }
-        return root.data + sum(root.left) + sum(root.right);
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
     }
     public static void main(String args[]){
         Node root = new Node(10);
         root.left = new Node(20);
         root.right = new Node(30);
+        root.left.left = new Node(40);
+        root.right.right = new Node(50);
 
-        System.out.println("sum = " + sum(root));
+        preorder(root);
     }
 }
